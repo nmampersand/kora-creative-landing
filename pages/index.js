50d7c14b227c59f7e83ada4475d1b449738d60d9
Nicole Ampersand <nmampersand@gmail.com>
2024-01-19 17:06:43 -0800

    add simple coming soon landing

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,16 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Kora Creative</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Kora Creative" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Coming Soon
         </p>
       </main>
-
-      <Footer />
     </div>
   )
 }
